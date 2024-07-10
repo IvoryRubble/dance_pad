7c5fc2c62fdc932da244961faf7c42b8d7fe30f5
Yaroslav Metelev <yaroslav.metelev@outlook.com>
2024-07-10 23:37:21 +0700

    Implemeted switch between keyboard and joystick modes

--- DancePad_6_buttons/src/main.cpp
+++ DancePad_6_buttons/src/main.cpp
@@ -9,14 +9,17 @@
 // A0 -   led gnd
 // A3 -   switch
 
+// two blinks on startup: gamepad mode
+// three blinks on startup: keyboard mode
+
 #include <Arduino.h>
+#include <Keyboard.h>
 #include <Joystick.h>
 #include "serialHelpers.h"
 #include "blinker.h"
 #include "buttonDebounce.h"
 #define LED A1
 #define LED_GND A0
-
 
 Blinker blinker;
 
@@ -31,6 +34,9 @@ const int inputGuardPin = 10;
 bool isInputEnabled = true;
 bool isButtonsReleased = false;
 
+const int switchPin = A3;
+bool switchState = false;
+
 ButtonDebounce buttonStartState;
 ButtonDebounce buttonSelectState;
 ButtonDebounce buttonRightState;
@@ -40,11 +46,12 @@ ButtonDebounce buttonLeftState;
 
 void updateStates();
 void setButtons();
-void setButton(int, ButtonDebounce);
+void setButton(int, ButtonDebounce, char);
 void updateBlinker();
 void checkInputGuard(); 
 void releaseButtons();
 void startupBlink();
+void switchBlink();
 
 // Set up joystick with 6 buttons, and disable hat switch & all axes
 Joystick_ Joystick( JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
@@ -53,6 +60,7 @@ Joystick_ Joystick( JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
 
 void setup() {
   Serial.begin(115200);
+  
   pinMode(LED, OUTPUT);
   pinMode(LED_GND, OUTPUT); 
 
@@ -62,14 +70,22 @@ void setup() {
   pinMode(buttonUpPin, INPUT_PULLUP);
   pinMode(buttonStartPin, INPUT_PULLUP);
   pinMode(buttonSelectPin, INPUT_PULLUP);
-
+  pinMode(switchPin, INPUT_PULLUP);
   pinMode(inputGuardPin, INPUT_PULLUP);
-
-  Joystick.begin();
 
   blinker.setPeriod(1000, 100);
 
   startupBlink();
+  delay(500);
+  
+  switchState = digitalRead(switchPin);
+  switchBlink();
+  delay(1000);
+  if (switchState) {
+    Joystick.begin();
+  } else {
+    Keyboard.begin();
+  }
 }
 
 void loop() {
@@ -84,7 +100,6 @@ void loop() {
 void checkInputGuard() {
   isInputEnabled = true;// !digitalRead(inputGuardPin);
   if (isInputEnabled) {
-    
     isButtonsReleased = false;
   } else {
     releaseButtons();
@@ -94,8 +109,12 @@ void checkInputGuard() {
 
 void releaseButtons() {
   if (isButtonsReleased) return;
-  for (int i = 0; i < 6; i++) {
-    Joystick.setButton(i, false);
+  if (switchState) {
+    for (int i = 0; i < 6; i++) {
+      Joystick.setButton(i, false);
+    }
+  } else {
+    Keyboard.releaseAll();
   }
 }
 
@@ -110,17 +129,25 @@ void updateStates() {
 
 void setButtons() {
   if (!isInputEnabled) return;
-  setButton(0, buttonStartState);
-  setButton(1, buttonSelectState);
-  setButton(2, buttonUpState);
-  setButton(3, buttonDownState);
-  setButton(4, buttonLeftState);
-  setButton(5, buttonRightState);
+  setButton(0, buttonStartState, KEY_RETURN);
+  setButton(1, buttonSelectState, KEY_ESC);
+  setButton(2, buttonUpState, KEY_UP_ARROW);
+  setButton(3, buttonDownState, KEY_DOWN_ARROW);
+  setButton(4, buttonLeftState, KEY_LEFT_ARROW);
+  setButton(5, buttonRightState, KEY_RIGHT_ARROW);
 }
 
-void setButton(int index, ButtonDebounce state) {
-  if (state.isBtnPressed || state.isBtnReleased) {
-    Joystick.setButton(index, state.btnState);
+void setButton(int index, ButtonDebounce state, char c) {
+  if (switchState) {
+    if (state.isBtnPressed || state.isBtnReleased) {
+      Joystick.setButton(index, state.btnState);
+    }
+  } else {
+    if (state.isBtnPressed) {
+      Keyboard.press(c);
+    } else if (state.isBtnReleased) {
+      Keyboard.release(c);
+    }
   }
 }
 
@@ -140,10 +167,28 @@ void updateBlinker() {
 }
 
 void startupBlink() {
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 20; i++) {
     digitalWrite(LED, HIGH);
-    delay(100);
+    delay(10);
     digitalWrite(LED, LOW);
-    delay(100);
+    delay(20);
+  }
+}
+
+void switchBlink() {
+  if (switchState) {
+    for (int i = 0; i < 2; i++) {
+      digitalWrite(LED, HIGH);
+      delay(200);
+      digitalWrite(LED, LOW);
+      delay(300);
+    }
+  } else {
+    for (int i = 0; i < 3; i++) {
+      digitalWrite(LED, HIGH);
+      delay(200);
+      digitalWrite(LED, LOW);
+      delay(300);
+    }
   }
 }
